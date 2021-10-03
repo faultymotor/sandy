c739a09c3f9e30707fcf017949fc60592cfc0d82
Rishi Nath <rishi.nath@gmail.com>
2021-10-02 22:38:42 -0700

    render 2d array to screen

--- index.py
+++ index.py
@@ -1,10 +1,44 @@
-from sandy import view
+import pygame
+import numpy as np
 
-def main():
-    window = view.View()
-    window.setup()
-    window.run()
+WIDTH, HEIGHT = 512, 256
+FPS = 60
+
+clock = pygame.time.Clock()
+ 
+pygame.init()
+pygame.display.set_caption('mapped')
+
+display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+
+def draw_array_to_screen(array, display):    
+    surface = pygame.surfarray.make_surface(array)
+    display.blit(surface, (0, 0))
 
 
-if __name__ == "__main__":
-    main()
+def color_monochrome_array(array, colorer):
+    width, height = np.shape(array)
+    dim = (width, height, 3)
+    colored_array = np.zeros(dim)
+
+    for x in range(width):
+        for y in range(height):
+            colored_array[x][y] = colorer(x, y)
+
+    return colored_array.astype('uint8')
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    array = np.random.rand(WIDTH, HEIGHT)
+    array = color_monochrome_array(np.random.rand(WIDTH, HEIGHT), lambda x, y: [array[x][y] * 255] * 3)
+    draw_array_to_screen(array, display)
+
+    pygame.display.update()
+    clock.tick(FPS)
+
